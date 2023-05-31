4d9d0ea51950f406f28c15ab63f65b3829596ea4
chaeiJeon <ahswn1031@gmail.com>
2023-05-31 19:09:14 +0900

    feat: Viewer 구현 WIP

--- src/components/FileUpload/index.jsx
+++ src/components/FileUpload/index.jsx
@@ -1,29 +1,77 @@
 import React, { useState } from "react";
-
 function FileUpload() {
   const [file, setFile] = useState(null);
+  let viewer;
 
   const onFileChange = (e) => {
     setFile(e.target.files[0]);
   };
+  function onDocumentLoadSuccess(doc) {
+    // A document contains references to 3D and 2D viewables.
+    var viewables = window.Autodesk.Viewing.Document.getSubItemsWithProperties(
+      doc.getRootItem(),
+      { type: "geometry" },
+      true
+    );
+    if (viewables.length === 0) {
+      console.error("Document contains no viewables.");
+      return;
+    }
 
+    // Choose any of the avialble viewables
+    var initialViewable = viewables[0];
+    var svfUrl = doc.getViewablePath(initialViewable);
+    var modelOptions = {
+      sharedPropertyDbPath: doc.getPropertyDbPath(),
+    };
+
+    var viewerDiv = document.getElementById("MyViewerDiv");
+    viewer = new window.Autodesk.Viewing.Private.GuiViewer3D(viewerDiv);
+    viewer.start(svfUrl, modelOptions, onLoadModelSuccess, onLoadModelError);
+  }
+  function onDocumentLoadFailure(viewerErrorCode) {
+    console.error("onDocumentLoadFailure() - errorCode:" + viewerErrorCode);
+  }
+  function onLoadModelSuccess(model) {
+    console.log("onLoadModelSuccess()!");
+    console.log("Validate model loaded: " + (viewer.model === model));
+    console.log(model);
+  }
+  function onLoadModelError(viewerErrorCode) {
+    console.error("onLoadModelError() - errorCode:" + viewerErrorCode);
+  }
   const onSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("dwg", file);
 
     try {
-      const response = await fetch("http://localhost:8080/upload", {
+      await fetch("http://localhost:8080/upload", {
         method: "POST",
         body: formData,
-      });
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          const { urn, token } = res.data;
+          console.log("translate success");
+          console.log(urn);
+          var options = {
+            env: "AutodeskProduction",
+            accessToken: token.access_token,
+          };
+          window.Autodesk.Viewing.Initializer(
+            options,
+            function onInitialized() {
+              window.Autodesk.Viewing.Document.load(
+                urn,
+                onDocumentLoadSuccess,
+                onDocumentLoadFailure
+              );
+            }
+          );
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      console.log(data);
+          return;
+        });
     } catch (err) {
       console.error("There has been a problem with your fetch operation:", err);
     }
